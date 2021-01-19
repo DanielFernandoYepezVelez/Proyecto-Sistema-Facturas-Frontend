ae9ad2668bc1b573d150aae3c922cc0d342d2b18
DanielFernandoYepezVelez <danipez.02@gmail.com>
2021-01-19 11:35:04 -0500

    Organizando Proyecto Inicial E Iniciando Los Componentes De Header Y Footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+
+/* Components */
 import { AppComponent } from './app.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { HeaderComponent } from './shared/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
